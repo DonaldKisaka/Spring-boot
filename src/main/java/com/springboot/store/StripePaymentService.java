14e71a2810279a04307f13dffcd0906e43ccec79
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-28 21:25:43 +0300

    dependency injection

---
+++ src/main/java/com/springboot/store/StripePaymentService.java
@@ -0,0 +1,10 @@
+package com.springboot.store;
+import com.springboot.PaymentService;
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIPE");
+        System.out.println("Amount: " + amount);
+    }
+}
