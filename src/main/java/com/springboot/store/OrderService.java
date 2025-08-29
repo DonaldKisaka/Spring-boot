0877160d3f770191601f485753233828562aa2f2
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-29 09:34:25 +0300

    setter injection

--- src/main/java/com/springboot/store/OrderService.java
+++ src/main/java/com/springboot/store/OrderService.java
@@ -1,8 +1,9 @@
 package com.springboot.store;
 
 public class OrderService {
-
     private PaymentService paymentService;
+
+
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
@@ -10,5 +11,9 @@ public class OrderService {
 
     public void placeOrder() {
         paymentService.processPayment(100);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
